b4deca4ca0f3682d0ac043269fcdeaf5fa2b32ef
ekayaint <ekayaint@gmail.com>
2023-06-11 19:41:58 +0300

    finished

--- SwiftUI-Animation-Trigger-DragGesture/ContentView.swift
+++ SwiftUI-Animation-Trigger-DragGesture/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState private var menuOffset : CGSize = .zero
+    
+    @State private var currentMenuY : CGFloat = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Triggers").font(.largeTitle)
+            Text("Drag Gesture").font(.title).foregroundColor(.gray)
+            
+            Spacer()
+            
+            VStack {
+                Circle().fill(Color.green)
+                    .frame(width: 100, height: 100)
+                    .overlay(Image(systemName: "line.horizontal.3").foregroundColor(.white)
+                        .offset(y: -20)
+                    )
+                    .offset(y: -50)
+                
+                HStack {
+                    Spacer()
+                }
+                Spacer()
+            }.frame(height: 200)
+                .background(RoundedRectangle(cornerRadius: 20).fill(Color.green))
+                .offset(y: currentMenuY + menuOffset.height)
+                .gesture(
+                    DragGesture()
+                        .updating($menuOffset, body: { (value, mOfs, transaction) in
+                            mOfs = value.translation
+                            
+                        })
+                        .onEnded({value in
+                            if value.translation.height > 100 {
+                                currentMenuY = 200 //closed
+                            } else {
+                                currentMenuY = 0 //open
+                            }
+                            
+                        })
+                )
+                .animation(.default, value: menuOffset)
+            
         }
-        .padding()
+        .font(.title)
     }
 }
 
