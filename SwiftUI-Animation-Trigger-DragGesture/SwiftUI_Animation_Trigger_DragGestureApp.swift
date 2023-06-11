2041321b266a816f6f6ce4cfdff1c66c61915adf
ekayaint <ekayaint@gmail.com>
2023-06-11 17:41:53 +0300

    Initial Commit

---
+++ SwiftUI-Animation-Trigger-DragGesture/SwiftUI_Animation_Trigger_DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_Trigger_DragGestureApp.swift
+//  SwiftUI-Animation-Trigger-DragGesture
+//
+//  Created by ekayaint on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_Trigger_DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
